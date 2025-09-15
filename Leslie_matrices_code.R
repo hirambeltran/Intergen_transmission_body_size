c9abaf24698b290e97a229465927ee0eded849b4
nestoraldea <nestor.aldea.ramos@gmail.com>
2025-09-15 18:32:12 +0200

    Add files via upload

--- Leslie_matrices_code.R
+++ Leslie_matrices_code.R
@@ -16,56 +16,38 @@ memory.limit(size=10^10)
 gen <- 10
 sim <- 20
 
-# array for stocking BMI distributions
-dis <- array(0:0, c(gen, 4, sim))
-
 #initial generation (derived from HRS couples, individuals
 # with PRS and BMI, restricted data)
-dt <- readRDS("~/your_folder/example_of_initial_generation.rds")
+data <- readRDS("~/ECHO/Obesity/Submission HH/Revision/2nd/example_of_initial_generation.rds")
 
 #add BMI category
-dt$cat <- ifelse(dt$bmi<18.5, 1,
-                 ifelse(dt$bmi<25, 2,
-                        ifelse(dt$bmi < 30,3,4)))
+data$cat <- ifelse(data$bmi<18.5, 1,
+                 ifelse(data$bmi<25, 2,
+                        ifelse(data$bmi < 30,3,4)))
 
 #number of omegas (AM parameter)
-oms <- 6
+oms <- 2
 #number of phis (DF paremeter)
-phs <- 3
+phs <- 2
 #number of ps (increased genetic penetrance)
 ps <- 3
 
+# array for stocking BMI distributions
+dis <- array(0:0, c(gen, 4, sim, oms, phs, ps))
 
-# Dataframe to stock results
-results <- as.data.frame(cbind(rep(seq(1, sim, by=1), each = gen * oms * phs * ps),
-                               rep(rep(seq(1, gen, by=1), each = oms * phs * ps), sim),
-                               rep(rep(seq(0, 1, by = 1/(oms-1)), each = phs * ps), sim * gen),
-                               rep(rep(seq(0, 1, by = 1/(phs-1)), each = ps), sim * gen * oms),
-                               rep(seq(0, (ps-1)*0.1, by = 0.1), sim * gen * oms * phs)))
-colnames(results) <- c("sim", "gen", "omega", "phi", "p")
-#to stock values
-results$bmi <- 0
-results$prs <- 0
-results$underw <- 0
-results$normal <- 0
-results$overw <- 0
-results$obese <- 0
-
-
-#stock initial values
-
-results$bmi [results$gen == 1] <- mean(dt$bmi)
-results$prs [results$gen == 1] <- mean(dt$pgs)
-
-results$underw [results$gen == 1] <- length(which(dt$cat == 1))/nrow(dt)
-results$normal [results$gen == 1] <- length(which(dt$cat == 2))/nrow(dt)
-results$overw [results$gen == 1] <- length(which(dt$cat == 3))/nrow(dt)
-results$obese [results$gen == 1] <- length(which(dt$cat == 4))/nrow(dt)
-
-for (sm in 1:sim){
-  dis [1,,sm] <- table(dt$cat)/nrow(dt)
+for (i in 1:oms){
+  for (j in 1:phs){
+    for (k in 1:ps){
+      for (sm in 1:sim){
+        dis [1,,sm, i, j, k] <- table(data$cat)/nrow(data)
+      }
+    }
+  }
 }
 
+
+#heritability matrix
+her <- array(0:0, c(16,4))
 
 #loop through parameter values
 
@@ -84,19 +66,31 @@ for (pp in 1:ps){
       #loop through simulations
       for (i in 1:sim){
         
+        dt <- data
+        
         #loop through generations
         for (t in 2:gen){
           
           #1--------------
           # Mating matrix
-          mat <- c(dis[t-1,1,i]*(dis[t-1,1,i]+(1-dis[t-1,1,i])*omega), dis[t-1,1,i]*dis[t-1,2,i]*(1-omega), 
-                   dis[t-1,1,i]*dis[t-1,3,i]*(1-omega), dis[t-1,1,i]*dis[t-1,4,i]*(1-omega),
-                   dis[t-1,2,i]*dis[t-1,1,i]*(1-omega), dis[t-1,2,i]*(dis[t-1,2,i]+(1-dis[t-1,2,i])*omega), 
-                   dis[t-1,2,i]*dis[t-1,3,i]*(1-omega), dis[t-1,2,i]*dis[t-1,4,i]*(1-omega),
-                   dis[t-1,3,i]*dis[t-1,1,i]*(1-omega), dis[t-1,3,i]*dis[t-1,2,i]*(1-omega), 
-                   dis[t-1,3,i]*(dis[t-1,3,i]+(1-dis[t-1,3,i])*omega), dis[t-1,3,i]*dis[t-1,4,i]*(1-omega),
-                   dis[t-1,4,i]*dis[t-1,1,i]*(1-omega), dis[t-1,4,i]*dis[t-1,2,i]*(1-omega), 
-                   dis[t-1,4,i]*dis[t-1,3,i]*(1-omega), dis[t-1,4,i]*(dis[t-1,4,i]+(1-dis[t-1,4,i])*omega))
+          bmis <- dis [t-1, , i, w, ph, pp]
+          
+          mat <- c(bmis[1]*(bmis[1]+(1-bmis[1])*omega), 
+                   bmis[1]*bmis[2]*(1-omega), 
+                   bmis[1]*bmis[3]*(1-omega), 
+                   bmis[1]*bmis[4]*(1-omega),
+                   bmis[2]*bmis[1]*(1-omega), 
+                   bmis[2]*(bmis[2]+(1-bmis[2])*omega), 
+                   bmis[2]*bmis[3]*(1-omega), 
+                   bmis[2]*bmis[4]*(1-omega),
+                   bmis[3]*bmis[1]*(1-omega), 
+                   bmis[3]*bmis[2]*(1-omega), 
+                   bmis[3]*(bmis[3]+(1-bmis[3])*omega), 
+                   bmis[3]*bmis[4]*(1-omega),
+                   bmis[4]*bmis[1]*(1-omega), 
+                   bmis[4]*bmis[2]*(1-omega), 
+                   bmis[4]*bmis[3]*(1-omega), 
+                   bmis[4]*(bmis[4]+(1-bmis[4])*omega))
           
           #2--------------
           # Fertility matrix (values for diagonal)
@@ -120,7 +114,7 @@ for (pp in 1:ps){
           #establish target couples of same BMI category
           tar <- array(0:0, c(4)) 
           for (c in 1:4){
-            tar [c] <- round(length(which(dt$cat==c)) * omega / 2, digits = 0)
+            tar [c] <- floor(length(which(dt$cat==c)) * omega / 2)
           }
           
           #couples dataset
@@ -142,10 +136,13 @@ for (pp in 1:ps){
           
           #make random couples
           howm <- floor(nrow(rest)/2)
-          who <- sample(1:nrow(rest), howm*2, replace = F)
-          for (ss in 1:howm){
-            coup <- rbind(coup, c(unlist(rest[who [ss], c("bmi","pgs", "cat")]),
-                                  unlist(rest[who [howm + ss], c("bmi","pgs", "cat")])))
+          
+          if (howm>0){
+            who <- sample(1:nrow(rest), howm*2, replace = F)
+            for (ss in 1:howm){
+              coup <- rbind(coup, c(unlist(rest[who [ss], c("bmi","pgs", "cat")]),
+                                    unlist(rest[who [howm + ss], c("bmi","pgs", "cat")])))
+            }
           }
           
           colnames(coup) <- c("bmi1", "pgs1", "cat1", "bmi2", "pgs2", "cat2")
@@ -181,7 +178,7 @@ for (pp in 1:ps){
           # 5. Compute heritability matrix 
           # (with parental and child BMI categories)
           #--------------------------------
-          her <- array(0:0, c(16,4))
+          
           
           for (z in 1:4){
             for (y in 1:4){
@@ -191,6 +188,28 @@ for (pp in 1:ps){
                 her [(z-1)*4 + y, 2] <- length(which(ww$cat == 2))/nrow(ww)
                 her [(z-1)*4 + y, 3] <- length(which(ww$cat == 3))/nrow(ww)
                 her [(z-1)*4 + y, 4] <- length(which(ww$cat == 4))/nrow(ww)
+              }else{
+                # if there are no individuals in this case and it's the first time
+                # step, children inherit the BMI class of their parents
+                if (t == 2){
+                  if (z == y){
+                    for (x in 1:4){
+                      if (x == z){
+                        her [(z-1)*4 + y, x] <- 1
+                      }else{
+                        her [(z-1)*4 + y, x] <- 0
+                      }
+                    }
+                  }else{
+                    for (x in 1:4){
+                      if (x %in% c(y,z)){
+                        her [(z-1)*4 + y, x] <- 0.5
+                      }else{
+                        her [(z-1)*4 + y, x] <- 0
+                      }
+                    }
+                  }
+                }
               }
               
             }
@@ -202,19 +221,16 @@ for (pp in 1:ps){
             obs [j] <- sum (child * her [,j])  
           }
           
-          dis[t, , i] <- obs / sum(obs)
-          
-          index <- which(results$sim == i & results$gen == t & results$omega == omega &
-                           results$phi == phi & results$p == p)
-          
-          #stock in results dataframe
-          
-          results$bmi [index] <- mean(new_gen$bmi)
-          results$prs [index] <- mean(new_gen$pgs)
-          results [index, 8:11] <- obs / sum(obs)
+          dis[t, , i, w, ph, pp] <- obs / sum(obs)
           
           #prepare for next time step
           dt <- new_gen [, c("pgs", "bmi", "cat")]
+          
+          #In order to reduce computation time, one can set a population limit 
+          # and randomly remove individuals over it.
+          if (nrow(dt) > 10000){
+            dt <- dt [sample(1:nrow(dt), 10000, replace = F),]
+          }
           
         }
       }
